68f3553fb4f14e89282d84864e941915008dbdad
DanteHung <143510962+DanteHung@users.noreply.github.com>
2024-03-19 17:35:12 +0800

    Add files via upload

---
+++ bcCryptoCoinGecko/src/main/java/bccryptoCoinGecko/bcCryptoCoinGecko/BcCryptoCoinGeckoApplication.java
@@ -0,0 +1,13 @@
+package bccryptoCoinGecko.bcCryptoCoinGecko;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BcCryptoCoinGeckoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BcCryptoCoinGeckoApplication.class, args);
+	}
+
+}
---
+++ bcCryptoCoinGecko/src/test/java/bccryptoCoinGecko/bcCryptoCoinGecko/BcCryptoCoinGeckoApplicationTests.java
@@ -0,0 +1,13 @@
+package bccryptoCoinGecko.bcCryptoCoinGecko;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BcCryptoCoinGeckoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
