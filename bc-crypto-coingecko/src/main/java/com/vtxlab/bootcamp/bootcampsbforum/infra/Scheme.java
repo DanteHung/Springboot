98500b176526eb535f9029367a81bd0c038f2d1e
DanteHung <143510962+DanteHung@users.noreply.github.com>
2024-03-19 17:32:33 +0800

    Delete bc-crypto-coingecko directory

--- bc-crypto-coingecko/src/main/java/com/vtxlab/bootcamp/bootcampsbforum/infra/Scheme.java
+++
@@ -1,9 +0,0 @@
-package com.vtxlab.bootcamp.bootcampsbforum.infra;
-
-public enum Scheme {
-  HTTPS, HTTP,;
-
-  public String toLowerCase() {
-    return this.name().toLowerCase(); // https or http
-  }
-}
